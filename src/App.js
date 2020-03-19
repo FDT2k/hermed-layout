67078a1a28e2ec0e39f4b6536c1291904ab590e0
Emilie K <emilie@karsegard.ch>
2020-03-19 17:59:00 +0100

    mise en place components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 //import './style';
-import './sass/style.scss';
+import 'sass/style.scss';
+import { Link, Route } from "wouter";
+import Catalogue from 'components/Catalogue'
+import Landing from 'components/CustomerLanding'
 
 function App() {
-  const version_string = `${process.env.GKA_FRONT_VERSION}-${process.env.NODE_ENV}`
+
   return (
-    <div className="customer-landing-page theme-light cover flex flex-column just-between">
-      <header className="">
-      </header>
-      <section className="flex flex-column align-center content-container">
-        <h1>Bienvenu</h1>
-        <p>
-          Le Dr. XXX vous invite à rejoindre sa salle d'attente pour une consultation à distance. Lorsque vous êtes prêts, cliquez sur le bouton ci-dessous pour la rejoindre.
-        </p>
-        <button>JE SUIS PRÊT !</button>
-      </section>
-      <footer class="footer">
-        <div>
-          <p alt={version_string} title={version_string}>2020 - Designed with love, cocoa and coffe by GeekAgency - Switzerland</p>
-        </div>
-      </footer>
-    </div>
+    <>
+    <Route path="/">
+
+    <section className="theme-light">
+    <h1>Menu</h1>
+    <ul>
+      <li><a href="/catalog" target="_blank"><h4>Catalogue</h4></a></li>
+      <li><a href="/CustomerLanding" target="_blank"><h4>Landing</h4></a></li>
+    </ul>
+    </section>
+    </Route>
+    <Route path="/catalog"><Catalogue/></Route>
+
+    </>
   );
 }
 
---
+++ src/components/Catalogue/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+import Exemple from 'components/Exemple'
+import Landing from 'components/CustomerLanding'
+
+export default props => {
+
+  return (
+    <section className="theme-default">
+      <Exemple/>
+      <Landing/>
+    </section>
+  )
+}
