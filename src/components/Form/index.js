171a9bcc96ceba665c895892ca476cd0618c89ff
Emilie K <emilie@karsegard.ch>
2020-03-20 23:43:04 +0100

    ctd

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,6 +4,15 @@ import React from 'react'
 export default props => {
 
   return (
-    <div></div>
+    <div>
+      <form className="basic-form" action="">
+
+        <div className="single-input flex flex-column">
+          <label htmlFor="">{label}</label>
+          <input className="" type="text" placeholder={placeholder}/>
+        </div>
+
+      </form>
+    </div>
   )
 }
