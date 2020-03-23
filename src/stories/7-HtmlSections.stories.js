2eaa348f112a658f9bfafd6deaa49e465cba6cb6
Emilie K <emilie@karsegard.ch>
2020-03-23 01:27:14 +0100

    start header components

---
+++ src/stories/7-HtmlSections.stories.js
@@ -0,0 +1,13 @@
+import React,{useEffect,useState,useRef} from 'react';
+import 'sass/style.scss';
+
+
+import Header from 'components/Header'
+
+export const SingleHeader =()=>
+    <div className="theme-chat"><Header/></div>
+
+
+export default {
+  title: 'Header',
+};
