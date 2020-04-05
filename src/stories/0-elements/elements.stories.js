0675b33f7161f0eaa0ba9ec9233d1da7852c794e
Fabien Di Tore <fabien@ditore.ch>
2020-04-05 23:26:56 +0200

    ctd

--- src/stories/0-elements/elements.stories.js
+++ src/stories/0-elements/elements.stories.js
@@ -1,11 +1,16 @@
 import React,{useEffect,useState,useRef} from 'react';
 import 'sass/style.scss';
-import ThemePicker from 'components/ThemePicker'
-import ChatBubble from 'components/ChatBubble'
 
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import {makeThemeSelect} from 'stories/theme-knobs'
+ 
+export default {
+  title: "Base",
+  decorators: [withKnobs]
+};
 
 export const  HTag =()=>
-    <ThemePicker picked="blue-light">
+    <div className={makeThemeSelect()}>
     <h1>H1 - Hello</h1>
     <h2>H2 - Hello</h2>
     <h3>H3 - Hello</h3>
@@ -19,8 +24,4 @@ export const  HTag =()=>
 
     <b>bold text</b>
     <i>bold text</i>
-    </ThemePicker>
-
-export default {
-  title: 'Base',
-};
+    </div>
